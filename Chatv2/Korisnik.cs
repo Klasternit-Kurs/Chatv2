373b793c14ade03c991eeb72a3a263a934d7429c
Dragan Pepic <d.pepic@hotmail.com>
2020-04-23 17:04:00 +0200

    Add project files.

---
+++ Chatv2/Korisnik.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatv2
+{
+	public class Korisnik
+	{
+		public string Ime { get; set; }
+		public List<string> PrimljenePoruke { get; set; }
+		public string Unos { get; set; }
+
+		public void PosaljiPoruku()
+		{
+			PrimljenePoruke.Add(Unos);
+			Unos = "";
+		}
+
+	}
+}
